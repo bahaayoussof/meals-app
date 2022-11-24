7a3cded9d10e342562e383c297eca644a23e9ffb
Bahaa Youssof <b.zaki@egyptsmartcards.com>
2022-11-24 12:09:52 +0200

    implement react navigation: stack

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import CategoriesScreen from "./screens/CategoriesScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
 	return (
 		<>
-			<StatusBar style="light" />
-			<CategoriesScreen />
+			<StatusBar style="dark" />
+			<NavigationContainer>
+				{/* Rest of your app code */}
+				<Stack.Navigator>
+					<Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+				</Stack.Navigator>
+			</NavigationContainer>
 		</>
 	);
 }
